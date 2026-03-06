329db85bfc4def6eae8d3c7a4543d3f2f4ac3007
Ceren <ccilt33@gmail.com>
2026-03-06 14:00:52 +0300

    3 farklı birim test daha eklendi

--- api_test/tests.py
+++ api_test/tests.py
@@ -31,3 +31,23 @@ class AuthTests(APITestCase):
         data = {"username": self.username, "password": "yanlis_sifre_123"}
         response = self.client.post(self.token_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_token_missing_fields(self):
+     data = {"username": self.username} 
+     response = self.client.post(self.token_url, data, format='json')
+     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+     self.assertIn('password', response.data)
+
+    def test_get_token_invalid_user(self):
+     data = {"username": "hayali_kullanici", "password": "sifre123"}
+     response = self.client.post(self.token_url, data, format='json')
+     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+def test_refresh_token_success(self):
+    login_data = {"username": self.username, "password": self.password}
+    login_response = self.client.post(self.token_url, login_data, format='json')
+    refresh_token = login_response.data['refresh']
+    refresh_url = reverse('token_refresh') 
+    response = self.client.post(refresh_url, {"refresh": refresh_token}, format='json')
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertIn('access', response.data)
