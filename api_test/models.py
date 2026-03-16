c0ad6bf9288c2283d3e7fcb2e26a83557087eb92
Ceren <ccilt33@gmail.com>
2026-03-16 14:03:21 +0300

    refactored

--- api_test/models.py
+++ api_test/models.py
@@ -1,38 +1,39 @@
 from django.db import models
 
 
-LANGUAGE_CHOICES = [
-    ("tr", "Turkish"),
-    ("en", "English"),
-    ("es", "Spanish"),
-    ("ar", "Arabic"),
-]
+
 
 class Content(models.Model):
 
-    title = models.CharField(max_length=150)
+    LANGUAGE_CHOICES = [
+        ("tr", "Turkish"),
+        ("en", "English"),
+        ("es", "Spanish"),
+        ("ar", "Arabic"),
+    ]
+    title = models.CharField(max_length=150, null=False, blank=False)
     desc = models.TextField(default="")
 
     cover = models.ImageField(upload_to="covers/", default="covers/default.jpg")
 
-    source = models.URLField()
+    source = models.URLField(null=False, blank=False)
     clean_source = models.URLField(null=True, blank=True)
 
     languages = models.CharField(
     max_length=2,
     choices=LANGUAGE_CHOICES
-)
+    )
 
-subtitles = models.CharField(
-    max_length=2,
-    choices=LANGUAGE_CHOICES,
-    blank=True
-)
+    subtitles = models.CharField(
+        max_length=2,
+        choices=LANGUAGE_CHOICES,
+        blank=True
+    )
 
-created = models.DateTimeField(auto_now_add=True)
-publish = models.DateTimeField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    publish = models.DateTimeField(null=True, blank=True)
 
-draft = models.BooleanField(default=True)
+    draft = models.BooleanField(default=True)
 
-def __str__(self):
-        return self.title
+    def __str__(self):
+            return self.title
